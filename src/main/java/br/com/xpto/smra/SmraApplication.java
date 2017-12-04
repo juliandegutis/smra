81d3eab904e7ef48434919fd344ef52077558668
Julian Degutis de Freitas Garcia <juliang@kyros.com.br>
2017-12-04 15:20:41 -0200

    Criação do Projeto

---
+++ src/main/java/br/com/xpto/smra/SmraApplication.java
@@ -0,0 +1,12 @@
+package br.com.xpto.smra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmraApplication.class, args);
+	}
+}
